777239b5c8045f41e2848729e243dd16db5d7fcf
Jintriaz <114219249+Jintriaz@users.noreply.github.com>
2023-02-08 10:55:42 +0100

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,5 @@
 <section>
-    Input a city in the search and get the current weather from the <a
-        href="https://openweathermap.org/"
-        target="_blank">OpenWeather API</a
-    >.
+    Bitte eine Stadt eintragen um das aktuelle Wetter dieser abzurufen.
 </section>
 
 <style>
